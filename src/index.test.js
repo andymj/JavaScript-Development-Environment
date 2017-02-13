c8d726ee0aa1520289a58aec3326c866df5abffd
Andy Mejia <andymej21@gmail.com>
2017-02-13 16:32:56 -0500

    added fake data and json-server

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('Our first text', () => {
 });
 
 describe('index.html', () => {
-    it('shoud say hello', (done) => {
+    it('h1 shoud say Users', (done) => {
         const index = fs.readFileSync('./src/index.html', "utf-8");
         jsdom.env(index, function(err, window) {
             const h1 = window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal('Hello World!');
+            expect(h1.innerHTML).to.equal('Users');
             done();
             window.close();
         });
